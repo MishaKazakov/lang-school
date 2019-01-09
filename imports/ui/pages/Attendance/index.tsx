ddfcd428fd6541cf916f29d429994c63f0223af4
Misha <miklelol@yandex.ru>
2019-01-09 16:05:36 +0700

    Добавляет api посешениям

--- imports/ui/pages/Attendance/index.tsx
+++ imports/ui/pages/Attendance/index.tsx
@@ -4,90 +4,48 @@ import StudentGroup from "../../components/StudentGroup";
 import { IStudent } from "../../../models/student";
 import { IEvent } from "../../../models/event";
 import { formatTime } from "../../../helpers/string";
+import * as qs from "query-string";
+
+import { Location } from "history";
+import { withRouter } from "react-router";
+
+import { compose } from "redux";
+import { withTracker } from "meteor/react-meteor-data";
+
+import { Events } from "../../../api/events";
+import { Students } from "../../../api/students";
+
+const Loader = require("antd/lib/spin");
 
 const cx = require("classnames/bind").bind(require("./style.scss"));
 
-const event: IEvent = {
-  _id: "1",
-  name: "Английский",
-  groupId: "abc",
-  date: new Date(),
-  timeStart: [10, 30],
-  timeEnd: [12, 0],
-  auditoryId: "wYAwfXSYeDMPgXv9w",
-};
+interface IProps {
+  students: IStudent[];
+  event: IEvent;
+  location: Location;
+}
 
-let students: IStudent[] = [
-  {
-    _id: "1",
-    firstName: "Дима",
-    lastName: "Васнецов",
-    group: {
-      abc: {
-        _id: "abc",
-        miss: [],
-        attended: [],
-        canceled: [],
-        numberLessons: 5
-      }
-    }
-  },
-  {
-    _id: "2",
-    firstName: "Вася",
-    lastName: "Савельев",
-    group: {
-      abc: {
-        _id: "abc",
-        miss: [],
-        attended: [],
-        canceled: [],
-        numberLessons: 5
-      }
-    }
-  },
-  {
-    _id: "3",
-    firstName: "Евгений",
-    lastName: "Онегин ",
-    group: {
-      abc: {
-        _id: "abc",
-        miss: [],
-        attended: [],
-        canceled: [],
-        numberLessons: 5
-      }
-    }
-  },
-  {
-    _id: "4",
-    firstName: "Захар",
-    lastName: "Медведев",
-    group: {
-      abc: {
-        _id: "abc",
-        miss: [],
-        attended: [],
-        canceled: ["1"],
-        numberLessons: 5
-      }
-    }
-  }
-];
-
-class Attendance extends React.Component {
+class Attendance extends React.Component<IProps> {
   mapGroup = (students: IStudent[], event) => {
     const group = [];
     const attended = [];
     const canceled = [];
-    const groupId: string = event.group;
+
+    if (!event || !students) {
+      return { group, attended, canceled };
+    }
+
+    const groupId: string = event.groupId;
     const eventId = event._id;
 
     students.forEach(student => {
-      if (student.group[groupId].attended.includes(eventId)) {
+      const studentGroup = student.group[groupId];
+      if (!studentGroup) {
+        return;
+      }
+      if (studentGroup.attended.includes(eventId)) {
         attended.push(student);
-      } else if (student.group[groupId].canceled.includes(eventId)) {
+      } else if (studentGroup.canceled.includes(eventId)) {
         canceled.push(student);
       } else {
         group.push(student);
@@ -104,6 +62,7 @@ class Attendance extends React.Component {
   };
 
   changeStudentStatus = (id: string) => {
+    const { event, students } = this.props;
     let student = students.find(student => student._id === id);
     const groupId = event.groupId;
     const group = student.group[groupId];
@@ -112,67 +71,84 @@ class Attendance extends React.Component {
     if (group.attended.includes(eventId)) {
       group.attended = group.attended.filter(id => id != eventId);
       group.canceled.push(eventId);
-      student.group[groupId] = group;
-      students = students.map(stud => {
-        if (stud._id === student._id) return student;
-        else return stud;
-      });
+      this.updateStudent(student, groupId, group);
       return;
     }
 
     if (group.canceled.includes(eventId)) {
       group.canceled = group.canceled.filter(id => id != eventId);
-      student.group[groupId] = group;
-      students = students.map(stud => {
-        if (stud._id === student._id) return student;
-        else return stud;
-      });
+      this.updateStudent(student, groupId, group);
       return;
     }
 
     group.attended.push(eventId);
+    this.updateStudent(student, groupId, group);
+  };
+
+  updateStudent = (student: IStudent, groupId, group) => {
     student.group[groupId] = group;
-    students = students.map(stud => {
-      if (stud._id === student._id) return student;
-      else return stud;
-    });
+    Students.update({ _id: student._id }, { ...student });
   };
 
   render() {
+    const { students, event } = this.props;
     const { group, attended, canceled } = this.mapGroup(students, event);
-    const time = formatTime(event.timeStart);
-    const d = event.date;
-    const day = `${d.getDay()}.${d.getMonth() + 1}.${d.getFullYear()}`;
+    const time = event && formatTime(event.timeStart);
+    const day = event && event.date;
+    const dayTittle =
+      event && `${day.getDay()}.${day.getMonth() + 1}.${day.getFullYear()}`;
+    const eventName = event ? event.name : "";
 
     return (
       <Layout>
-        <div className={cx("attendance")}>
-          <div className={cx("attendance__title")}>
-            Отметка посещаемости {event.name}
+        {event && students ? (
+          <div className={cx("attendance")}>
+            <div className={cx("attendance__title")}>
+              Отметка посещаемости {eventName}
+            </div>
+            <div className={cx("attendance__day")}>Дата: {dayTittle}</div>
+            <div className={cx("attendance__day")}>Время: {time}</div>
+            <div className={cx("attendance__groups")}>
+              <StudentGroup
+                onClick={this.onClick}
+                title="Студенты в группе"
+                students={group}
+              />
+              <StudentGroup
+                onClick={this.onClick}
+                title="Пришли"
+                students={attended}
+              />
+              <StudentGroup
+                onClick={this.onClick}
+                title="Отменили"
+                students={canceled}
+              />
+            </div>
           </div>
-          <div className={cx("attendance__day")}>Дата: {day}</div>
-          <div className={cx("attendance__day")}>Время: {time}</div>
-          <div className={cx("attendance__groups")}>
-            <StudentGroup
-              onClick={this.onClick}
-              title="Студенты в группе"
-              students={group}
-            />
-            <StudentGroup
-              onClick={this.onClick}
-              title="Пришли"
-              students={attended}
-            />
-            <StudentGroup
-              onClick={this.onClick}
-              title="Отменили"
-              students={canceled}
-            />
+        ) : (
+          <div className={cx("attendance__loader")}>
+            <Loader />
           </div>
-        </div>
+        )}
       </Layout>
     );
   }
 }
 
-export default Attendance;
+export default compose(
+  withRouter,
+  withTracker<any, IProps>(({ location }) => {
+    const _id = location && qs.parse(location.search)["id"];
+    const event: any = Events.findOne({ _id });
+    const groupId = event && event.groupId;
+    const students =
+      event &&
+      Students.find({ [`group.${groupId}`]: { $exists: true } }).fetch();
+
+    return {
+      event,
+      students
+    };
+  })
+)(Attendance);
